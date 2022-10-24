c86df3feacd654e582c76f861700066bf788e3ba
Ariel Retamales <nretamales15@gmail.com>
2022-10-24 12:20:03 -0300

    repojava

---
+++ src/cl/generation/f20221024/Main.java
@@ -0,0 +1,19 @@
+package cl.generation.f20221024;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		
+		Integer as = 12 ; 
+		
+		Float a = Float.valueOf(as)	;			
+				
+		System.out.println(as);
+		
+		
+		
+	}
+
+}
